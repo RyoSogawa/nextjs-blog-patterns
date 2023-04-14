b77fefec882b16d661105891442be5f54d4a8a03
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-14 23:36:25 +0900

    feat: create top page

--- src-project/src/pages/index.tsx
+++ src-project/src/pages/index.tsx
@@ -10,13 +10,21 @@ export default function Home() {
     <div className={`${inter.className} container p-24`}>
       <h1 className="text-center text-5xl">Blog List</h1>
       <ul className={`${inter.className} mt-20`}>
-        {newsList.map((news) => (
-          <li key={news.id} className="list-disc mb-4">
-            <Link href={`/news/${news.id}`} className="text-blue-500">
-              {news.title}
-            </Link>
-          </li>
-        ))}
+        <li className="list-disc mb-4">
+          <Link href="/csr" className="text-blue-500">
+            CSR
+          </Link>
+        </li>
+        <li className="list-disc mb-4">
+          <Link href="/ssr" className="text-blue-500">
+            SSR
+          </Link>
+        </li>
+        <li className="list-disc mb-4">
+          <Link href="/ssg" className="text-blue-500">
+            SSG
+          </Link>
+        </li>
       </ul>
     </div>
   );
