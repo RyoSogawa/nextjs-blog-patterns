950cb8ed7229c7de68bfbea6f152315cfe415883
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-14 22:51:06 +0900

    feat: rsc single page

---
+++ rsc/src/app/news/[id]/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import fetchHKNews from '@/services/fetchHKNews';
+import { Inter } from 'next/font/google';
+import React from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default async function News({ params }: { params: { id: string } }) {
+  const news = await fetchHKNews(params.id);
+  return (
+    <div className={`${inter.className} container p-24`}>
+      <h1 className="text-center text-5xl">Blog Single</h1>
+      <Link href={'/'} className="text-blue-500 mt-8 block">
+        ←To Top
+      </Link>
+      {news && <pre className="mt-20">{JSON.stringify(news, null, 2)}</pre>}
+    </div>
+  );
+}
