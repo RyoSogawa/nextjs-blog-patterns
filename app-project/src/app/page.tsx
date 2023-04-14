4d4246f39dcc9f7bf417990446f9c428d8613df9
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-14 23:53:07 +0900

    feat: improve top

--- app-project/src/app/page.tsx
+++ app-project/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import fetchHKNewsList from '@/services/fetchHKNewsList';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -7,18 +6,20 @@ const inter = Inter({ subsets: ['latin'] });
 export const revalidate = 60;
 
 export default async function Home() {
-  const newsList = await fetchHKNewsList();
   return (
     <div className={`${inter.className} container p-24`}>
       <h1 className="text-center text-5xl">Blog List</h1>
       <ul className={`${inter.className} mt-20`}>
-        {newsList.map((news) => (
-          <li key={news.id} className="list-disc mb-4">
-            <Link href={`/news/${news.id}`} className="text-blue-500">
-              {news.title}
-            </Link>
-          </li>
-        ))}
+        <li className="list-disc mb-4">
+          <Link href="/rsc" className="text-blue-500">
+            RSC
+          </Link>
+        </li>
+        <li className="list-disc mb-4">
+          <Link href="/static-export" className="text-blue-500">
+            Static Export
+          </Link>
+        </li>
       </ul>
     </div>
   );
