53e102a2dbce9e912f9beab3869f23674a7956ef
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-14 23:46:24 +0900

    feat: remove ssg project

--- ssg/src/services/fetchHKNews.ts
+++
@@ -1,5 +0,0 @@
-export default async function fetchHKNews(id: string) {
-  if (!id) return;
-  const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
-  return await res.json();
-}
